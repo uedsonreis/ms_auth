a6e3b9540cd7c03fbdb4fedd914f5ebe1f62b6e9
Uedson Reis <uedson.reis@sovos.com>
2022-09-15 16:47:58 -0300

    WIP

--- settings.py
+++ settings.py
@@ -8,10 +8,10 @@ db_url = 'localhost' if db_url is None else db_url
 db_port = environ.get('DATABASE_PORT')
 db_port = '5432' if db_port is None else db_port
 
-db_user = environ.get('DATABASE_USER')
+db_user = environ.get('DATABASE_USER') # ms_auth
 db_user = 'postgres' if db_user is None else db_user
 
-db_password = environ.get('DATABASE_PASSWORD')
+db_password = environ.get('DATABASE_PASSWORD') # Ms4uth@db
 db_password = 'reis' if db_password is None else db_password
 
 [app, db] = create_app(f'postgresql://{db_user}:{db_password}@{db_url}:{db_port}/ms_auth', True, True)
